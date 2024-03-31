baef6c66c4bbacac56df74a6eff4233b53a748ab
SEUNG RAK <seungrak@SEUNGs-MacBook-Pro.local>
2024-03-31 14:58:39 +0900

    Modify Add setter and use bean value

--- spring/src/main/java/com/studies/spring/di/Program.java
+++ spring/src/main/java/com/studies/spring/di/Program.java
@@ -8,7 +8,6 @@ import com.studies.spring.di.ui.ExamConsole;
 public class Program {
 
     public static void main(String[] args) {
-        
 
         /* 스프링에게 지시하는 방법으로 코드를 변경
         Exam exam = new NewlecExam();
